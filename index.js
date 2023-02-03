7e4e23f36e7be725b5e5f036df125efa4a773d01
Azhar Sayyed <azhartsiddiqui@gmail.com>
2023-02-03 18:23:37 +0530

    Oprating System Module

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-const express = require("express");
-const multer = require("multer");
-
-const app = express();
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + "-" + Date.now() + ".jpg");
-    },
-  }),
-}).single("fileName");
-
-app.post("/upload", upload, (req, resp) => {
-  resp.send({ msg: "file upload" });
-});
-
-app.listen(4000);
+const os = require("os");
+// console.log(os.arch());
+// const freeMemory = os.freemem();
+// const totalMemory = os.freemem();
+// console.log(freeMemory / (1024 * 1024 * 1024));
+// console.log(totalMemory / (1024 * 1024 * 1024));
+// console.log(os.hostname());
+// console.log(os.platform());
+console.log(os.userInfo());
